a500475b82581b90106b92b9721d3c470972c5fb
datnguyen <datnguyen012153@gmail.com>
2025-10-25 11:16:25 +0700

    feat: create verification service

---
+++ verification-service/src/main/java/com/example/verificationservice/verify_request/VerifyRequest.java
@@ -0,0 +1,39 @@
+package com.example.verificationservice;
+
+
+import com.example.commondto.constant.VerifyStatus;
+import com.example.commondto.constant.VerifyType;
+import com.example.commondto.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "verify_request")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VerifyRequest extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    private VerifyType type; // VEHICLE, JOURNEY, CREDIT
+
+    private Long referenceId;
+
+    private String title;
+
+    private String description;
+
+    private String documentUrl;
+
+    @Enumerated(EnumType.STRING)
+    private VerifyStatus status; // PENDING, APPROVED, REJECTED
+
+}
---
+++ verification-service/src/main/java/com/example/verificationservice/verify_request/VerifyRequestRepository.java
@@ -0,0 +1,4 @@
+package com.example.verificationservice;
+
+public interface VerifyRequestRepository {
+}
